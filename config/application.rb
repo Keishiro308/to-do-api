6da4541cb4ef085a701086b396cc04ce5a47d0bf
Keishiro Nishiyama <kn11308@gmail.com>
2019-12-18 19:32:12 +0900

    commit

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Todo
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'https://react-redux-todo-sample.herokuapp.com/'
         resource '*',
         :headers => :any,
         :methods => [:get, :post, :patch, :delete, :update, :put]
